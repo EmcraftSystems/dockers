a892b38bfd18b9d01c34cb6f9d76b560b53ee86d
Sergei Poselenov <sposelenov@emcraft.com>
2023-09-12 16:00:17 +0000

    RM-6816: Docker file for Emcraft 6ULL Yocto (1.8.1) and BSP builds.
    See https://emcraft.com/som/imx-6ull/using-prebuilt-container-image-for-development

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,3 @@
-# Build docker, passing current uid and gid and SSH keys to the docker image.
+docker build -t ubuntu14-6ull:latest yocto-docker/
+#--build-arg UID="$(id -u)" --build-arg GID="$(id -g)"  --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" 
 
-if [ ! -f yocto-docker/mcuxpressoide-tools-11.0.1.201907311258.tar.bz2 ]; then
-    echo "Please download mcuxpressoide-tools-11.0.1.201907311258.tar.bz2 to yocto/docker/"
-    exit -1
-fi
-
-docker build -t lgo-docker:0.1 yocto-docker --build-arg UID="$(id -u)" --build-arg GID="$(id -g)"  --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" 
--- docker-run.sh
+++ docker-run.sh
@@ -1,20 +1,14 @@
 # Work directory, will be mapped to /work on docker, for example
-# WORKDIR=/work/psl/IMX8M/work/RM5990-docker/work-docker
-# Where Yocto sstate cache is, could be an empty dir, for example
-# SSTATE_DIR=/work/yocto/sstate-cache
-# Where Yocto downloads cache is, could be an empty dir, for example
-# DL_DIR=/work/yocto/Downloads
+WORKDIR=/work/psl/IMX8M/work/RM5990-docker/work-docker
 
 
-if [ -z "${WORKDIR}" ] || [ -z "$SSTATE_DIR" ] || [ -z "${DL_DIR}" ]; then
-    echo "Set SSTATE_DIR, WORKDIR and DL_DIR"
+
+if [ -z "${WORKDIR}" ]; then
+    echo "WORKDIR is not set, exit"
     exit -1
 fi
 
-docker run --rm -i -t --name mydocker 	--security-opt "label=disable" \
+docker run -i -t  --security-opt "label=disable" \
        -v ${WORKDIR}:/work \
-       -v ${WORKDIR}/opt:/opt:z \
-       -v ${SSTATE_DIR}:/opt/yocto/sstate-cache:z \
-       -v ${DL_DIR}:/opt/yocto/downloads:z \
        --env="DISPLAY" \
-       lgo-docker:0.1
+       ubuntu14-6ull:latest
